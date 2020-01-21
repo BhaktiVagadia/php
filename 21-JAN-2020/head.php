e98ebe6a5dafd399b80781db693a2fa5abe9f3b0
Bhakti <bhakti.vagadia@gmail.com>
2020-01-21 15:25:36 +0530

    Practice of 21-JAN

--- 21-JAN-2020/head.php
+++ 21-JAN-2020/head.php
@@ -1,9 +1,9 @@
 <h3>This is from Head file</h3>
 <?php
-    $firstName = "Bhakti";
+    /*$firstName = "Bhakti";
     function Addition($num1,$num2){
         echo "<br>From Function Addition of head file";
         echo "<br>Addition : " . ($num1 + $num2);
     }
-    Addition(10,20);
+    Addition(10,20);*/
 ?>
